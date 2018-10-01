7ca45475429bfe2f93d94af4248a8a1f4afaf7fe
Sarit <sarit891@gmail.com>
2018-10-01 14:58:45 +0300

    First init

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,2 @@
 // Global app controller
 
-import num from './test';
-
-console.log(`I imported ${num} from another module`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
-const path = require('path');
+const path = require('path'),
+    htmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['idempotent-babel-polyfill', './src/js/index.js'],
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
-    mode: 'development'
-}
+    devServer: {
+        // all of the apps code is inside the dist folder
+        // thus the src folder is only for dev purposes
+        // once we build the app its all merged to the bundle.js 
+        contentBase: './dist'
+    },
+    plugins: [
+        // allow us to do complex processing of our input files (index.js)
+        // it copies the src/html to the dist/js/html
+        new htmlWebPackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    // setting loaders, such as babel
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    }
+};
